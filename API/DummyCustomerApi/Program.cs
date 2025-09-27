83a793a3d50f3132dd78930ddc78d9b45634b8d9
Aditi Agarwal <aditiagarwal852@gmail.com>
2025-09-27 07:22:13 +0530

    Program.cs
    API/DummyCustomerApi/Program.cs

---
+++ API/DummyCustomerApi/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "DummyCustomerApi",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+// Enable swagger in Development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DummyCustomerApi v1");
+    });
+}
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
